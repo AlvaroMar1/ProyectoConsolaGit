e74722f633419343f2053610216c740af9536ea2
Alvaro Marques <alvmarmor6@alu.edu.gva.es>
2026-01-08 11:58:33 +0100

    Añadido Operaciones.java (multiplicar y dividir) con rama FuncionesBasicas

---
+++ src/Operaciones.java
@@ -0,0 +1,9 @@
+public class Operaciones {
+    public void multiplicar(int num1, int num2) {
+        int resultado = num1 * num2;
+    }
+
+    public void dividir(int num1, int num2) {
+        int resultado = num1 / num2;
+    }
+}
