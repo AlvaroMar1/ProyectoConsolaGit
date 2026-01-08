eadeeaa23f2804918661eda39503b78f1e5c1446
Alvaro Marques <alvmarmor6@alu.edu.gva.es>
2026-01-08 12:07:48 +0100

    Archivo MensajeOperaciones.java (Bienvenida y despedida) creado en la rama Mensajes

---
+++ src/MensajeOperaciones.java
@@ -0,0 +1,9 @@
+public class MensajeOperaciones {
+    public void bienvenida() {
+        System.out.println("Bienvenido");
+    }
+
+    public void despedida() {
+        System.out.println("Hasta luego");
+    }
+}
